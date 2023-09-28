f2fce5b278bbeda3c3d9df2a61acc8d0a5263f09
Ville Korpi <ville.korpi@perkinelmer.com>
2023-09-28 12:21:32 +0300

    account-ui header clean-up

--- js/apps/account-ui/src/root/Root.tsx
+++ js/apps/account-ui/src/root/Root.tsx
@@ -1,77 +1,29 @@
-import { Button, Page, Spinner } from "@patternfly/react-core";
-import {
-  KeycloakMasthead,
-  Translations,
-  TranslationsProvider,
-} from "keycloak-masthead";
-import { Suspense, useMemo } from "react";
-import { useTranslation } from "react-i18next";
-import { Outlet, useHref } from "react-router-dom";
+import { Page, Spinner, Brand, PageHeader } from "@patternfly/react-core";
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
 import { AlertProvider } from "ui-shared";
 
-import { ExternalLinkSquareAltIcon } from "@patternfly/react-icons";
 import { environment } from "../environment";
-import { keycloak } from "../keycloak";
-import { joinPath } from "../utils/joinPath";
 import { PageNav } from "./PageNav";
 
-import style from "./Root.module.css";
-
-const ReferrerLink = () => {
-  const { t } = useTranslation();
-  const searchParams = new URLSearchParams(location.search);
-
-  return searchParams.has("referrer_uri") ? (
-    <Button
-      component="a"
-      href={searchParams.get("referrer_uri")!.replace("_hash_", "#")}
-      variant="link"
-      icon={<ExternalLinkSquareAltIcon />}
-      iconPosition="right"
-      isInline
-    >
-      {t("backTo", { app: searchParams.get("referrer") })}
-    </Button>
-  ) : null;
-};
-
 export const Root = () => {
-  const { t } = useTranslation();
-  const brandImage = environment.logo || "/SIMA23.svg";
-  const logoUrl = environment.logoUrl ? environment.logoUrl : "/";
-  const internalLogoHref = useHref(logoUrl);
-
-  // User can indicate that he wants an internal URL by starting it with "/"
-  const indexHref = logoUrl.startsWith("/") ? internalLogoHref : logoUrl;
-
-  const translations = useMemo<Translations>(
-    () => ({
-      avatar: t("avatar"),
-      fullName: t("fullName"),
-      manageAccount: t("manageAccount"),
-      signOut: t("signOut"),
-      unknownUser: t("unknownUser"),
-    }),
-    [t],
-  );
+  const logo = environment.logo || "/SIMA23.svg";
 
   return (
     <Page
       header={
-        <TranslationsProvider translations={translations}>
-          <KeycloakMasthead
-            features={{ hasManageAccount: false }}
-            showNavToggle
-            brand={{
-              href: indexHref,
-              src: joinPath(environment.resourceUrl, brandImage),
-              alt: t("logo"),
-              className: style.brand,
-            }}
-            toolbarItems={[<ReferrerLink key="link" />]}
-            keycloak={keycloak}
-          />
-        </TranslationsProvider>
+        <PageHeader
+          showNavToggle
+          logo={
+            <Brand
+              src={environment.resourceUrl + logo}
+              id="masthead-logo"
+              alt="Logo"
+              className="keycloak__pageheader_brand"
+            />
+          }
+          logoComponent="div"
+        />
       }
       sidebar={<PageNav />}
       isManagedSidebar
